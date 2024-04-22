4a2af883b21dcaefabfd989352d6bf06c8258441
PokemonOwner <160018170+PokemonOwner@users.noreply.github.com>
2024-04-22 09:31:01 -0700

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -6,7 +6,8 @@ from main.plugins.progress import progress_for_pyrogram
 from main.plugins.helpers import screenshot
 from pyrogram import Client, filters
 from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid, PeerIdInvalid
-from pyrogram.enums import MessageMediaType
+from pyrogram.types import MessageMediaType
+from telethon import events
 from ethon.pyfunc import video_metadata
 from ethon.telefunc import fast_upload
 from telethon.tl.types import DocumentAttributeVideo
